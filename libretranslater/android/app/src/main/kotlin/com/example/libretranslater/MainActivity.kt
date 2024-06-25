9f8d5e3188f9f8fc7ad65e4cf1c9b97f45d6e6a5
mesteranas <135442710+mesteranas@users.noreply.github.com>
2024-06-25 18:17:43 +0300

    25/06/2024

---
+++ libretranslater/android/app/src/main/kotlin/com/example/libretranslater/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.libretranslater
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ libretranslater/lib/app.dart
@@ -0,0 +1,7 @@
+class App{
+  static const name="libre translater";
+  static const appName="libretranslate_flutter";
+  static const appDirName="libretranslate";
+  static const version=1.0;
+  static const description="a app makes you to translate given text";
+}
